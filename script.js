7201a653d67ac3363508afe74b64122eeacece2e
unknown <mylene.obejero@g.msuiit.edu.ph>
2026-01-28 19:39:51 +0800

    Adding new html

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-// Typing Effect
 const text = "Full Stack Developer";
 let index = 0;
-const typingElement = document.getElementById("typing");
 
 function typeEffect() {
   if (index < text.length) {
-    typingElement.textContent += text.charAt(index);
+    document.getElementById("typing").textContent += text.charAt(index);
     index++;
-    setTimeout(typeEffect, 100);
+    setTimeout(typeEffect, 80);
   }
 }
-typeEffect();
 
-// Scroll Reveal
-const hiddenElements = document.querySelectorAll(".project-card, .skills");
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-hiddenElements.forEach(el => {
-  el.classList.add("hidden");
-  observer.observe(el);
-});
+window.onload = typeEffect;
